b3bc7ba4bc94460f8a2be698e5c33a8cedba63a4
eyob j <jemijobs22@gmail.com>
2025-02-23 13:43:01 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./componets/Header"
+import Footer from "./componets/Footer"
+import Home from "./componets/Home"
+function App(){
+    
+    return <>
+        <Header />        
+        <Home />
+        <Footer />
+
+    </>
+}
+
+
+
+
+export default App
